cd0583df5ab01917d4e4b1da050a9a17165e7198
NaishengZhang <44213750+NaishengZhang@users.noreply.github.com>
2019-11-15 17:20:48 -0500

    Initial commit

---
+++ src/main/java/edu/nyu/cs9053/homework9/Factory.java
@@ -0,0 +1,15 @@
+package edu.nyu.cs9053.homework9;
+
+/**
+ * User: blangel
+ */
+public class Factory {
+
+    public static Customer createCustomer() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Barista createBarista() {
+        throw new UnsupportedOperationException();
+    }
+}
